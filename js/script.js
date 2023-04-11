c0c55479d190fb61301756be4ff18ac6f93059bc
greeenery <07_mz@naver.com>
2023-04-11 19:02:35 +0900

    230411 반응형 확인용 업로드

--- js/script.js
+++ js/script.js
@@ -29,13 +29,13 @@ function slide__Init() {
     pagination: {
       el: '.main-slide .swiper-pagination',
       clickable: true,
-    }
+    },
 
   });
 
   // section-1 slide
   const sectionslide = new Swiper('.section-1__slide .swiper', {
-    slidesPerView: 4.8,
+    slidesPerView: 1.5,
     centeredSlides: true,
     spaceBetween: 40,
     // If we need pagination
@@ -53,6 +53,21 @@ function slide__Init() {
     scrollbar: {
       el: '.section-1__slide .swiper-scrollbar',
     },
+
+    breakpoints: {
+      768: {
+        slidesPerView: 2.5,
+        spaceBetween: 40
+      },
+      1024: {
+        slidesPerView: 3.5,
+        spaceBetween: 40
+      },
+      1280: {
+        slidesPerView: 4.5,
+        spaceBetween: 40
+      },
+    }
   });
 }
 
