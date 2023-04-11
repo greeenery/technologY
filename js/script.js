52b4623f82d67d25e99412ff3544c9fb249397ca
greeenery <07_mz@naver.com>
2023-04-11 18:06:25 +0900

    230411 간단한 애니메이션 및 정리 약간

--- js/script.js
+++ js/script.js
@@ -1,37 +1,59 @@
-// main-slide
-const swiper = new Swiper('.main-slide .swiper', {
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: '.main-slide .swiper-pagination',
-  },
+function changeHeaderColor() {
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.main-slide .swiper-button-next',
-    prevEl: '.main-slide .swiper-button-prev',
-  },
+  $(window).scroll(function (event) {
+    let st = $(this).scrollTop();
+    if (st > 0) {
+      $("header").addClass("black");
+    } else {
+      $("header").removeClass("black");
+    }
+  });
 
-});
+}
 
-// main-slide
-const swiper2 = new Swiper('.section-1__slide .swiper', {
-  slidesPerView:4.8,
-  centeredSlides:true,
-  spaceBetween:40,
-  // If we need pagination
-  pagination: {
-    el: '.section-1__slide .swiper-pagination',
-  },
+changeHeaderColor();
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.section-1__slide .swiper-button-next',
-    prevEl: '.section-1__slide .swiper-button-prev',
-  },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.section-1__slide .swiper-scrollbar',
-  },
-});
+function slide__Init() {
+  // main-slide
+  const mainslide = new Swiper('.main-slide .swiper', {
+    loop: true,
+    speed: 1000,
+
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.main-slide .swiper-pagination',
+      clickable: true,
+    }
+
+  });
+
+  // section-1 slide
+  const sectionslide = new Swiper('.section-1__slide .swiper', {
+    slidesPerView: 4.8,
+    centeredSlides: true,
+    spaceBetween: 40,
+    // If we need pagination
+    pagination: {
+      el: '.section-1__slide .swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.section-1__slide .swiper-button-next',
+      prevEl: '.section-1__slide .swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.section-1__slide .swiper-scrollbar',
+    },
+  });
+}
+
+slide__Init();
